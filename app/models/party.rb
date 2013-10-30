343702bf87c72ab40cd00920d8f75fadcbbf8a93
mindfulcoder <jodie@mindfulcoder.com>
2013-10-30 12:41:26 +0000

    Fix Merge Conflicts

--- app/models/party.rb
+++ app/models/party.rb
@@ -6,15 +6,9 @@ class Party < ActiveRecord::Base
 	has_many :date_options, :dependent => :destroy
 	has_many :participants, :dependent => :destroy
 	
-<<<<<<< HEAD
 	accepts_nested_attributes_for :budgets, allow_destroy: true, reject_if: lambda {|attributes| attributes['amount'].blank?}
 	accepts_nested_attributes_for :date_options, allow_destroy: true, reject_if: lambda {|attributes| attributes['start_date'].blank?}
 	accepts_nested_attributes_for :participants, allow_destroy: true, reject_if: lambda {|attributes| attributes['email'].blank?}
-=======
-	accepts_nested_attributes_for :budgets, allow_destroy: true
-	accepts_nested_attributes_for :date_options, allow_destroy: true
-	accepts_nested_attributes_for :participants, allow_destroy: true
->>>>>>> 20432586578cb33c1dd0b576ca55f479ddc075b2
 
 	def assign_chief_hen(user)
 		self.user = user
