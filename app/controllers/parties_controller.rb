ec2a61ad00291a786cd8b9c73eb1802d290a118c
Harrisam <samantha.s.harris@googlemail.com>
2013-11-04 16:12:13 +0000

    add best price to plan page

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -10,6 +10,7 @@ class PartiesController < ApplicationController
                                    :invitation,
                                    :send_invitations,
                                    :plan]
+  before_action :get_best_price_pages, only: [:plan]
   helper_method :party_and_user_errors
   helper_method :send_email
 
@@ -153,6 +154,31 @@ class PartiesController < ApplicationController
 
     def response_params
       params.require(:response).permit(:date_option_ids)
+    end
+
+    def get_best_price_pages
+      product_ids = { kit:          '02aecf7f1570ea714fdc00bf581ee66c',
+                      shera:        'ff1cb21c78cfcaaddf58e445d2f24b56',
+                      gay_bf:       '1f96b711b43040e94ebaa0ff42ac0195',
+                      l_plates:     '18688049702bbfcc4f90602b57191970',
+                      penis_straws: 'cd3c4f335954d2b5171ce0265871f96d' }
+      products = product_ids.values.map { |id| product_details(id) }
+
+      @best_price_pages = []
+      products.each do |product|
+        @best_price_pages << product.pages.min_by { |page| page.price + page.pnp  }
+      end
+    end
+
+    def invisible_hand_api
+      InvisibleHand::API.new :app_id => ENV['INVISIBLE_HAND_APP_ID'],
+                             :app_key => ENV['INVISIBLE_HAND_APP_KEY'],
+                             :region => 'uk'
+    end
+
+    def product_details(product_id)
+      @api ||= invisible_hand_api
+      @api.product(product_id)
     end
 
 end
