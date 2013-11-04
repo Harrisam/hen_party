2e1483d4589ae1f7161a8e6fdc9be130602ce458
Harrisam <samantha.s.harris@googlemail.com>
2013-11-04 15:45:40 +0000

    Select cheapest retailer for each product and styled as thumbnails

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,10 +3,17 @@ require 'invisiblehand'
 class ProductsController < ApplicationController
 
   def index
-    product_ids = { shera:    'ff1cb21c78cfcaaddf58e445d2f24b56',
-                    gay_bf:   '1f96b711b43040e94ebaa0ff42ac0195',
-                    l_plates: '18688049702bbfcc4f90602b57191970' }
-    @products = product_ids.values.map { |id| product_details(id) }
+    product_ids = { kit:          '02aecf7f1570ea714fdc00bf581ee66c',
+                    shera:        'ff1cb21c78cfcaaddf58e445d2f24b56',
+                    gay_bf:       '1f96b711b43040e94ebaa0ff42ac0195',
+                    l_plates:     '18688049702bbfcc4f90602b57191970',
+                    penis_straws: 'cd3c4f335954d2b5171ce0265871f96d' }
+    products = product_ids.values.map { |id| product_details(id) }
+
+    @best_price_pages = []
+    products.each do |product|
+      @best_price_pages << product.pages.min_by { |page| page.price + page.pnp  }
+    end
   end
 
   def invisible_hand_api
--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,14 @@
 module ProductsHelper
+
+  def format_price(page)
+    total = number_to_currency((page.price + page.pnp), precision: 2)
+
+    if page.pnp > 0
+      pnp = number_to_currency(page.pnp, precision: 2)
+    else
+      pnp = "free P&P"
+    end
+
+    "#{total} (inc. #{pnp})"
+  end
 end
